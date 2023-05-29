ea2606322b4c31f0cf51479108b3bfaf735c3952
KiyoshiTakeshi <126044513+KiyoshiTakeshi@users.noreply.github.com>
2023-05-29 21:23:49 +0700

    Add files via upload

--- cayvl/src/main/java/module-info.java
+++ cayvl/src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module sample.cayvl {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.sql;
+
+
     opens sample to javafx.fxml;
     exports sample;
 }
